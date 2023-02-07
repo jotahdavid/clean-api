35e681364bf1c36339450d95a082162aa7a9b71f
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-02-07 19:02:44 -0300

    feat: setup express server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,8 @@
-console.log('Hello world!');
+import app from './app';
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`✅ Server is running at http://localhost:${port}`);
+});
