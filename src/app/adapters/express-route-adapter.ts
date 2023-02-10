64e57f6fedc6ab8f16d7635b32d7bb361ddc2f6b
Jhonatan David <82552187+jotahdavid@users.noreply.github.com>
2023-02-10 17:22:23 -0300

    feat: create express adapter

---
+++ src/app/adapters/express-route-adapter.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+
+import { HttpRequest, HttpRequestHandler } from '../shared/http';
+
+export class ExpressRouteAdapter {
+  static adapt(requestHandler: HttpRequestHandler) {
+    return async (req: Request, res: Response) => {
+      const httpRequest: HttpRequest = {
+        body: req.body,
+      };
+
+      try {
+        const httpResponse = await requestHandler(httpRequest);
+        return res.status(httpResponse.statusCode).json(httpResponse.body);
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error(err);
+        return res.sendStatus(500);
+      }
+    };
+  }
+}
